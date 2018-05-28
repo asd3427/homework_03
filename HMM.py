4e351cd6d93f70f017c7f53366eb116686032e1d
asd3427 <34855528+asd3427@users.noreply.github.com>
2018-05-28 21:49:29 +0800

    做了很多很多的測試 結果 失敗

--- HMM.py
+++ HMM.py
@@ -14,22 +14,24 @@ for i in data:
     for a in i:
         n.append(a)
     l.append(n)
-l=np.array(l)
+l= np.array(l)
 
 n_state = ['1','2','3']
 observations=['a','b','c','d']
 n_observations = len(observations)
 cc = hmm.GaussianHMM(n_components=n_state,startprob_prior=l,transmat_prior=l)
-cc.fit(l)
-inpuT = np.array([x for x in input().split()])
-ll=[]
+print(cc)
 
-for i in inpuT:
-    seen = []
-    for a in i:
-        seen.append(a)
-    seen.append(n)
-logprob, box = cc.decode(seen, algorithm="viterbi")
-
-print("The ball picked:", ", ".join(map(lambda x: observations[x], seen)))
-print("The hidden box", ", ".join(map(lambda x: states[x], box)))
+cc.fit()
+#inpuT = np.array([x for x in input().split()])
+#ll=[]
+#
+#for i in inpuT:
+#    seen = []
+#    for a in i:
+#        seen.append(a)
+#    seen.append(n)
+#logprob, box = cc.decode(seen, algorithm="viterbi")
+#
+#print("The ball picked:", ", ".join(map(lambda x: observations[x], seen)))
+#print("The hidden box", ", ".join(map(lambda x: states[x], box)))
